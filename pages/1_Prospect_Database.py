17ee9c2c8cf12b14f97e53bd9cd40aca7eee0634
Boris Crnjak <boriscrnjak@outlook.com>
2025-09-20 04:44:13 +0000

    Prototype 1

--- pages/1_Prospect_Database.py
+++ pages/1_Prospect_Database.py
@@ -1,10 +1,21 @@
-import json
 import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
 import pandas as pd
 import ast
-from streamlit_app import sheet
+
+scope = ["https://spreadsheets.google.com/feeds",
+         "https://www.googleapis.com/auth/drive"]
+
+creds_dict = dict(st.secrets["gcp_service_account"])
+creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")
+
+creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
+client = gspread.authorize(creds)
+
+sheet = client.open("rec_database").worksheet("players")
+data = sheet.get_all_records()
+df = pd.DataFrame(data)
 
 st.header("UNC MBB Prospect Database")
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,2 @@
 import streamlit as st
-import gspread
-from google.oauth2.service_account import Credentials
-import pandas as pd
-
 st.switch_page("pages/1_Prospect_Database.py")
-
-scope = ["https://spreadsheets.google.com/feeds",
-         "https://www.googleapis.com/auth/drive"]
-
-creds_dict = dict(st.secrets["gcp_service_account"])
-creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")
-
-creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
-client = gspread.authorize(creds)
-
-sheet = client.open("rec_database").worksheet("players")
-data = sheet.get_all_records()
-df = pd.DataFrame(data)
