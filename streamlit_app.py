3b92d917debb9b22359f36d8ac8670348cc77620
Boris Crnjak <boriscrnjak@outlook.com>
2025-09-20 03:43:44 +0000

    -

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,47 +4,18 @@ import gspread
 from google.oauth2.service_account import Credentials
 import pandas as pd
 import ast
-import streamlit_authenticator as stauth
 
-users = {
-    "usernames": {
-        "bcrnjak": {"name": "John Doe", "password": "frontoffice23"},
-        "jtanner": {"name": "Jane Smith", "password": "frontoffice23"},
-        "tbeisner": {"name": "Tom Beisner", "password": "frontoffice23"}
-    }
-}
+st.switch_page("pages/1_Prospect_Database.py")
 
-authenticator = stauth.Authenticate(
-    users,
-    "my_cookie_name",
-    "my_signature_key",
-    cookie_expiry_days=1
-)
+scope = ["https://spreadsheets.google.com/feeds",
+         "https://www.googleapis.com/auth/drive"]
 
-login_result = authenticator.login(location='main')
+creds_dict = dict(st.secrets["gcp_service_account"])
+creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")
 
-if login_result is not None:
-    name, authentication_status, username = login_result
-else:
-    st.warning("Login widget did not render. Please check your Streamlit setup.")
-    st.stop()
+creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
+client = gspread.authorize(creds)
 
-if authentication_status:
-    st.switch_page("pages/1_Prospect_Database.py")
-    scope = ["https://spreadsheets.google.com/feeds",
-             "https://www.googleapis.com/auth/drive"]
-
-    creds_dict = dict(st.secrets["gcp_service_account"])
-    creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")
-
-    creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
-    client = gspread.authorize(creds)
-
-    sheet = client.open("rec_database").worksheet("players")
-    data = sheet.get_all_records()
-    df = pd.DataFrame(data)
-elif authentication_status == False:
-    st.error("Username/password is incorrect")
-elif authentication_status is None:
-    st.warning("Please enter your username and password")
-
+sheet = client.open("rec_database").worksheet("players")
+data = sheet.get_all_records()
+df = pd.DataFrame(data)
