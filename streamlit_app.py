201ba54ee48ab962c7f82d42e494b0a74fbc0147
Boris Crnjak <boriscrnjak@outlook.com>
2025-09-20 03:42:13 +0000

    idk

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,19 +4,47 @@ import gspread
 from google.oauth2.service_account import Credentials
 import pandas as pd
 import ast
+import streamlit_authenticator as stauth
 
-st.switch_page("pages/1_Prospect_Database.py")
+users = {
+    "usernames": {
+        "bcrnjak": {"name": "John Doe", "password": "frontoffice23"},
+        "jtanner": {"name": "Jane Smith", "password": "frontoffice23"},
+        "tbeisner": {"name": "Tom Beisner", "password": "frontoffice23"}
+    }
+}
 
-scope = ["https://spreadsheets.google.com/feeds",
-         "https://www.googleapis.com/auth/drive"]
+authenticator = stauth.Authenticate(
+    users,
+    "my_cookie_name",
+    "my_signature_key",
+    cookie_expiry_days=1
+)
 
-creds_dict = dict(st.secrets["gcp_service_account"])
-creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")
+login_result = authenticator.login(location='main')
 
-creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
-client = gspread.authorize(creds)
+if login_result is not None:
+    name, authentication_status, username = login_result
+else:
+    st.warning("Login widget did not render. Please check your Streamlit setup.")
+    st.stop()
 
-sheet = client.open("rec_database").worksheet("players")
-data = sheet.get_all_records()
-df = pd.DataFrame(data)
+if authentication_status:
+    st.switch_page("pages/1_Prospect_Database.py")
+    scope = ["https://spreadsheets.google.com/feeds",
+             "https://www.googleapis.com/auth/drive"]
+
+    creds_dict = dict(st.secrets["gcp_service_account"])
+    creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")
+
+    creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
+    client = gspread.authorize(creds)
+
+    sheet = client.open("rec_database").worksheet("players")
+    data = sheet.get_all_records()
+    df = pd.DataFrame(data)
+elif authentication_status == False:
+    st.error("Username/password is incorrect")
+elif authentication_status is None:
+    st.warning("Please enter your username and password")
 
