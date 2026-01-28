599ca8ec40f7bd43858bd7f4bab7eda5b4ad85e5
Boris Crnjak <boriscrnjak@outlook.com>
2026-01-28 21:53:46 +0000

    fixing cache

--- db_utils.py
+++ db_utils.py
@@ -4,16 +4,29 @@ from google.oauth2.service_account import Credentials
 import pandas as pd
 import json
 
-scope = ["https://spreadsheets.google.com/feeds",
-         "https://www.googleapis.com/auth/drive"]
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
 
-creds_dict = dict(st.secrets["gcp_service_account"])
-creds_dict = json.loads(st.secrets["gcp_service_account"]["service_account_json"])
-creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")
+@st.cache_resource
+def get_gspread_client():
+    creds_dict = json.loads(
+        st.secrets["gcp_service_account"]["service_account_json"]
+    )
+    creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")
 
-creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
-client = gspread.authorize(creds)
+    creds = Credentials.from_service_account_info(
+        creds_dict,
+        scopes=scope
+    )
+    return gspread.authorize(creds)
 
-sheet = client.open("rec_database").worksheet("players")
-data = sheet.get_all_records()
-df = pd.DataFrame(data)
+@st.cache_data(ttl=300)  # refresh every 5 minutes
+def load_sheet():
+    client = get_gspread_client()
+    sheet = client.open("rec_database").worksheet("players")
+    data = sheet.get_all_records()
+    return pd.DataFrame(data)
+
+df = load_sheet()
